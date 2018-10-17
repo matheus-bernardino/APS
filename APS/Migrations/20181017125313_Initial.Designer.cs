e0effcbf505ce3e1c615db2f7839906118509673
matheus-bernardino <matheus.bernardino@aluno.uece.br>
2018-10-17 10:56:09 -0300

    Corrige bugs e erros de interface

--- APS/Migrations/20181017125313_Initial.Designer.cs
+++ APS/Migrations/20181017125313_Initial.Designer.cs
@@ -11,7 +11,7 @@ using System;
 namespace APS.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20181016230858_Initial")]
+    [Migration("20181017125313_Initial")]
     partial class Initial
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
